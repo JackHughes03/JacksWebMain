9e1aed1c4742ed41e951641685ed56e67e36fe5b
JackHughes03 <101028724+JackHughes03@users.noreply.github.com>
2023-10-08 19:27:31 +0100

    test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Vue from 'vue'; // Change to './vue' or '../vue' if needed
+import ../Vue from 'vue'; // Change to './vue' or '../vue' if needed
 const selectedSlideIndex = ref(0),
 showAbout = ref(false);
 
