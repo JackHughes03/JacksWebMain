3dafcea011c2dac31e0462d089ebd75326455dd1
JackHughes03 <jwhughes03@yahoo.com>
2023-10-08 20:19:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-    publicPath: process.env.NODE_ENV === "production" ? "/JacksWebMain/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 })
